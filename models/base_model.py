4461685967fb3e0b5faeead1a50ae790f80fc241
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-06 01:28:34 -0800

    Edit state.py, base_model.py and city.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 from sqlalchemy import String, Column, DATETIME
 from sqlalchemy.ext.declarative import declarative_base
+from models import storage_type
 
 
 Base = declarative_base()
@@ -45,7 +46,7 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascad="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """
+            returns the list of City instances
+            with state_id equals to the current State.id
+            """
+            from models import storage
+            related = []
+            cities = storage.all(City)
+            for city in cities:
+                if city.state_id == self.id:
+                    related.append(city)
+            return (related)
