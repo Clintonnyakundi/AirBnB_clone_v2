6e6b72591c3dde7beb8ec54be9537dac757d7221
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-01-06 00:03:46 -0800

    Fix script to have have correct password

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- create database
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 -- create a new user
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_db';
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- grant all privileges on hbnb_test_db database
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 -- reload grant tables
