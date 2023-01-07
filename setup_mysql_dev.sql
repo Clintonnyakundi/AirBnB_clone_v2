a26c421e43836ffa3fcc40ed5dbaa62cc941f87c
Anne Wachana <96468875+a11y-2824@users.noreply.github.com>
2023-01-07 04:54:34 +0300

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,5 @@
---Create a MySQL server 
+-- Create a MySQL server
+-- It creates a project developement database
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
