9891d79de00f3d85b497fdbbdefa585862dd2aa4
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-04 05:00:44 -0800

    Write a script that starts a flask web app with root and hbnb routes

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Starts a flask web app
+Listen on 0.0.0.0 port 5000
+/: display 'Hello HBNB!'
+use option strict_slashes=False in route
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_route():
+    """
+    Displays 'Hello HBNB!'
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_route():
+    """
+    Displays 'HBNB'
+    """
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
