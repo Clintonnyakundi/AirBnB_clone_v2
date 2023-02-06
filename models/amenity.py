674762400a0986694f088c43091f0a6ce79cd43e
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-06 12:48:17 -0800

    Edit Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -8,9 +8,8 @@ from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
     """Represents an Amenity"""
-
-    __tablename__ = "amenities"
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ == "amenities"
         name = Column(String(128), nullable=False, default="")
         place_amenities = relationship(
             "Place", secondary="place_amenity", viewonly=False)
