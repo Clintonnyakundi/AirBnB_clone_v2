dd2314e340f12149502d79e6b0810469b461f8e3
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-06 12:51:42 -0800

    Fix assignment of __tablename__

--- models/amenity.py
+++ models/amenity.py
@@ -9,7 +9,7 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Represents an Amenity"""
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ == "amenities"
+        __tablename__ = "amenities"
         name = Column(String(128), nullable=False, default="")
         place_amenities = relationship(
             "Place", secondary="place_amenity", viewonly=False)
