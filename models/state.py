95870b3dad5dc0cce28d7db41b957983e7a30436
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-06 13:08:23 -0800

    Fix State class

--- models/state.py
+++ models/state.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
-from os import getenv
 from models.city import City
-from sqlalchemy import Column, String
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """Representation of state """
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City",  backref="state",
-                              cascade="all, delete, delete-orphan")
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            from models import storage
+            """getter for list of city instances related to the state"""
             city_list = []
-            for city in list(storage.all(City).values()):
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
