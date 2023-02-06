37682dc7b22b7f77c6c57f3cfd755103b5f35c55
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-06 10:59:48 -0800

    Write script that starts flask app and displays cities by states

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web app
+Listens on 0.0.0.0 port 5000
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Loads cities of a state"""
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove current SQLAlchemy session"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
