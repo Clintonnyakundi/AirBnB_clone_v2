b22da468cbab45f00aed98f8a3efa77d22e7e1da
Clintonnyakundi <mokayaclinton34@gmail.com>
2023-02-07 02:11:39 -0800

    Add changes

--- models/review.py
+++ models/review.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
-import os
+""" Review Class File """
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import ForeignKey
 
 
 class Review(BaseModel, Base):
-    """ Review class to store review information """
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'reviews'
-        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-        text = Column(String(1024), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """ Define Review Class
+        __tablename__: reviews
+        text: Column String(1024) can't be null
+        place_id: Column String(60) ForeignKey to places.id can't be null
+        user_id: Column String(60) ForeignKey to users.id can't be null
+    """
+    __tablename__ = "reviews"
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
--- models/user.py
+++ models/user.py
@@ -1,26 +1,30 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+""" User Class File """
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-        places = relationship('Place', backref='user',
-                              cascade='all, delete, delete-orphan')
-        reviews = relationship('Review', backref='user',
-                               cascade='all, delete, delete-orphan')
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """ Define User Class
+        __tablename__: users
+        email: Column String(128) can't be null
+        password: Column String(128) can't be null
+        first_name: Column String(128) can be null
+        last_name: Column String(128) can be null
+        places: relationship with Place, if the User object
+                deleted all linked Place object must be
+                automatically delted, and the reference name is user
+        reviews: relationship with Review, if the User object
+                 deleted all linked Review object must be deleted
+                 automatically, and the reference name is user
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
